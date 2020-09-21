6f2a6b9240594d632ea8627b227a3b5b73dd5288
Josh Wallace <walliby84@gmail.com>
2020-09-20 20:24:53 -0400

    finish collection item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <button
+    type="button"
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{
@@ -13,7 +19,7 @@ const MenuItem = ({ title, imageUrl, size }) => (
       <h1 className="title">{title}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
-  </div>
+  </button>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
