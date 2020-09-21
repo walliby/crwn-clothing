cb56feb59be2452df0a1f3e3968610871cefa30f
Josh Wallace <walliby84@gmail.com>
2020-09-21 16:04:39 -0400

    start firebase

---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './form-input.styles.scss';
+
+const FormInput = ({ handleChange, label, id, ...otherProps }) => (
+  <div className="group">
+    <input
+      id={id}
+      className="form-input"
+      onChange={handleChange}
+      {...otherProps}
+    />
+    {label && (
+      <label
+        htmlFor={id}
+        className={`${otherProps.value.length && 'shrink'} form-input-label`}
+      >
+        {label}
+      </label>
+    )}
+  </div>
+);
+
+export default FormInput;
