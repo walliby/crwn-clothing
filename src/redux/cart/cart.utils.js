106e1983921407279bdc15bf8322d0a691767020
Josh Wallace <walliby84@gmail.com>
2020-10-02 12:47:16 -0400

    add and remove items from cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -15,3 +15,21 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== existingCartItem.id);
+  }
+
+  return cartItems.map((cartItem) => {
+    if (cartItem.id === existingCartItem.id) {
+      return { ...cartItem, quantity: cartItem.quantity - 1 };
+    }
+
+    return cartItem;
+  });
+};
